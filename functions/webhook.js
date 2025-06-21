cf66c0a3e3a8b6c92887a18a97b786004c1a6b79
Potato228 <petruhakabachok@gmail.com>
2025-06-21 21:35:56 +0800

    fixes again

--- functions/webhook.js
+++ functions/webhook.js
@@ -1,23 +1,24 @@
-require('dotenv').config();
-const TelegramBot = require('node-telegram-bot-api');
+require("dotenv").config();
+const TelegramBot = require("node-telegram-bot-api");
 
 const token = process.env.TELEGRAM_BOT_TOKEN;
 const bot = new TelegramBot(token, { webHook: true }); // webhook mode!
+bot.setWebHook(`${process.env.URL}/api/bot`);
 
 bot.onText(/\/start/, (msg) => {
-  bot.sendMessage(msg.chat.id, 'Бот успешно запущен на Netlify 🏥');
+	bot.sendMessage(msg.chat.id, "Бот успешно запущен на Netlify 🏥");
 });
 
-exports.handler = async function(event) {
-  if (event.httpMethod !== 'POST') {
-    return { statusCode: 200, body: 'OK' };
-  }
-  try {
-    const body = JSON.parse(event.body);
-    await bot.processUpdate(body); // ✨ handle Telegram update
-    return { statusCode: 200, body: '' };
-  } catch (err) {
-    console.error('Webhook error:', err);
-    return { statusCode: 500, body: 'Webhook error' };
-  }
+exports.handler = async function (event) {
+	if (event.httpMethod !== "POST") {
+		return { statusCode: 200, body: "OK" };
+	}
+	try {
+		const body = JSON.parse(event.body);
+		await bot.processUpdate(body); // ✨ handle Telegram update
+		return { statusCode: 200, body: "" };
+	} catch (err) {
+		console.error("Webhook error:", err);
+		return { statusCode: 500, body: "Webhook error" };
+	}
 };
