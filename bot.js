72e87a8d6e030fc3cd20237f3d9b5115c18a3f48
Potato228 <petruhakabachok@gmail.com>
2025-06-22 20:37:13 +0800

    q

--- bot.js
+++ bot.js
@@ -138,11 +138,11 @@ async function forwardQuestion(chatID) {
 }
 
 // Pictures
-const image_label_path = path.join(__dirname, "..", "media", "images", "label.jpg");
-const image_label = fs.createReadStream(image_label_path);
+const image_label_path = path.join(__dirname, "media", "images", "label.jpg");
+const image_label = fs.createReadStream("./media/images/label.jpg");
 
-const image_map_path = path.join(__dirname, "..", "media", "images", "label.jpg");
-const image_map = fs.createReadStream(image_map_path);
+const image_map_path = path.join(__dirname, "media", "images", "label.jpg");
+const image_map = fs.createReadStream("./media/images/map.jpg");
 
 // Receive message
 bot.on("message", async (msg) => {
